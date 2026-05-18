8f235283ac8e442a5903f9ecd57addd87d350037
ほし <43630612+hoshi2710@users.noreply.github.com>
2026-05-18 18:50:43 +0900

    test

--- dist/index.js
+++ dist/index.js
@@ -25,7 +25,7 @@ app.post("/api/v1/store/review", handleCreateReview);
 app.post("/api/v1/store/mission", handleCreateMission);
 app.post("/api/v1/store/mission/state", handleChallengeMission);
 app.get("/api/v1/stores/:storeId/reviews", handleListStoreReviews);
-app.get("/api/v1/users/:userId/reviews", handleListUserReviews);
+app.get("/api", handleListUserReviews);
 app.get("/api/v1/stores/:storeId/mission", handleListStoreMissions);
 app.get("/api/v1/users/:userId/mission", handleListUserMissions);
 app.patch("/api/v1/mission/:userId/:missionId", handleChangeMissionState);
